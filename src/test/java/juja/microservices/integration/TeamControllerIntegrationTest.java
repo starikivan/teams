2be3eadd3191cab16c2213384ccc4277730825d6
starikivan <peter.v.kramar@gmail.com>
2017-07-09 20:26:17 +0300

    test travis

---
+++ src/test/java/juja/microservices/integration/TeamControllerIntegrationTest.java
@@ -0,0 +1,14 @@
+package juja.microservices.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class TeamControllerIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void test() {
+
+    }
+}
---
+++ src/test/java/juja/microservices/integration/TeamServiceIntegrationTest.java
@@ -0,0 +1,14 @@
+package juja.microservices.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class TeamServiceIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void test() {
+
+    }
+}
