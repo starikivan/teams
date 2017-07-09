6ae6cca23eadde68d4882704a520c1f856577250
starikivan <peter.v.kramar@gmail.com>
2017-07-09 19:32:24 +0300

    test travis

--- src/test/java/juja/microservices/acceptance/TeamsAcceptanceTest.java
+++ src/test/java/juja/microservices/acceptance/TeamsAcceptanceTest.java
@@ -1,9 +1,14 @@
 package juja.microservices.acceptance;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TeamsAcceptanceTest extends BaseAcceptanceTest{
 
+    @Test
+    public void test() {
+
+    }
 }
