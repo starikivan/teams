2b05c3dda9b964c6969c445e947372f5b67da0cf
asidun <asidun@gmail.com>
2017-07-10 16:36:44 +0300

    #6: Made TeamRequest class

--- src/main/java/juja/microservices/teams/entity/TeamRequest.java
+++ src/main/java/juja/microservices/teams/entity/TeamRequest.java
@@ -1,4 +1,33 @@
 package juja.microservices.teams.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@ToString
 public class TeamRequest {
+
+    @Id
+    private String id;
+    private String from;
+    private String uuidOne;
+    private String uuidTwo;
+    private String uuidThree;
+    private String uuidFour;
+
+    @JsonCreator
+    public TeamRequest(@JsonProperty("from") String from,
+                @JsonProperty("uuidOne") String uuidOne,
+                @JsonProperty("uuidTwo") String uuidTwo,
+                @JsonProperty("uuidThree") String uuidThree,
+                @JsonProperty("uuidFour") String uuidFour) {
+        this.from = from;
+        this.uuidOne = uuidOne;
+        this.uuidTwo = uuidTwo;
+        this.uuidThree = uuidThree;
+        this.uuidFour = uuidFour;
+    }
 }
