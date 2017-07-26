e7d5516a984e16fe3434d9ad8a303cf88cf59fe9
Ivan Shapovalov <ivshapovalov@gmail.com>
2017-07-26 22:34:03 +0300

    #28 - fix mistakes

--- src/main/java/ua/com/juja/microservices/teams/exceptions/UserAlreadyInTeamException.java
+++ src/main/java/ua/com/juja/microservices/teams/exceptions/UserAlreadyInTeamException.java
@@ -3,6 +3,6 @@ package ua.com.juja.microservices.teams.exceptions;
 /**
  * Created by Andrii Sidun
  */
-public class UserAlreadyInTeamException extends RuntimeException {
+public class UserAlreadyInTeamException extends TeamsException {
     public UserAlreadyInTeamException(String message) {super(message);}
 }
--- src/test/java/ua/com/juja/microservices/teams/dao/TeamRepositoryTest.java
+++ src/test/java/ua/com/juja/microservices/teams/dao/TeamRepositoryTest.java
@@ -36,9 +36,6 @@ public class TeamRepositoryTest extends BaseIntegrationTest {
 
     @Test
     public void test_saveTeamExecutedCorrectly() {
-        String a = "asdfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", b =
-                "dsdfsafsafsafsaffasfsfsfsafsafafaf";
-        String c = a + b;
         final String userInOneTeam = "user-in-one-team";
         final String userInSeveralTeams = "user-in-several-teams";
         final Team expected =
